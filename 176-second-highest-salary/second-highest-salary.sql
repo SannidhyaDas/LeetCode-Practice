4a314ed5ad1750e42156a741fd1e8fe830b8c1b8
Sannidhya   Das <150328685+SannidhyaDas@users.noreply.github.com>
2026-03-23 15:24:29 +0530

    Time: 268 ms (52.12%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -2,4 +2,4 @@
 #SELECT salary AS SecondHighestSalary from Employee LIMIT 1,1
 ## Test case 2 failed by above code.
 
-SELECT max(salary) AS SecondHighestSalary from Employee e1 WHERE salary < (SELECT max(salary) from Employee) 
+SELECT max(salary) AS SecondHighestSalary from Employee e1 WHERE salary < (SELECT max(salary) from Employee); 
